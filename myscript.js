2c16a40e4a6a91eeb722abb979846d2b12ce961e
NoeFabris <noe@elysresidence.com>
2021-04-07 16:01:53 +0200

    Js Pt.1

---
+++ myscript.js
@@ -0,0 +1,5 @@
+var name = prompt("Insert your name");
+var surname = prompt("Now your surname");
+var favColor = prompt("Now your favorite colour");
+
+document.getElementById('pw-gen').innerHTML=name + surname + color
